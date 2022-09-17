c59fd17548571ad5aa44d1df8f4ca0f350c82502
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-17 16:21:53 +0300

    finished the script

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 window.addEventListener('load', function(){
+    const number=document.getElementById("number")
+    const increase=document.getElementById("increase")
+    const decrease=document.getElementById("decrease")
+    const reset=document.getElementById("reset")
+    let score=0;
+    const addOne=()=>{
+        score=score+1;
+        number.innerHTML=score;
+    }
 
+    increase.addEventListener("click",addOne)
 
+    const minusOne=()=>{
+        score=score-1;
+        number.innerHTML=score;
+    }
 
+    decrease.addEventListener("click",minusOne)
     
+
+    const resetZero=()=>{
+        score=0;
+        number.innerHTML=score;
+    }
+
+    reset.addEventListener("click",resetZero)
 })
